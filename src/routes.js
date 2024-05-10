15de01cac690356e5f8581995d6a019445bed233
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2024-05-10 23:06:06 +0000

    finish the books feature

--- src/routes.js
+++ src/routes.js
@@ -36,6 +36,26 @@ const addSchema = Joi.object({
   reading: Joi.boolean().required(),
 });
 
+const editSchema = Joi.object({
+  name: Joi.string()
+    .required()
+    .error(new Error(`Gagal memperbarui buku. Mohon isi nama buku`)),
+  year: Joi.number().optional(),
+  author: Joi.string().optional(),
+  summary: Joi.string().optional(),
+  publisher: Joi.string().optional(),
+  pageCount: Joi.number().optional(),
+  readPage: Joi.number()
+    .max(Joi.ref("pageCount"))
+    .optional()
+    .error(
+      new Error(
+        "Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount",
+      ),
+    ),
+  reading: Joi.boolean().optional(),
+});
+
 const routes = [
   {
     method: "POST",
@@ -47,6 +67,32 @@ const routes = [
         failAction,
       },
     },
+  },
+  {
+    method: "GET",
+    path: "/books",
+    handler: getAllBooksHandler,
+  },
+  {
+    method: "GET",
+    path: "/books/{id}",
+    handler: getBookByIdHandler,
+  },
+  {
+    method: "PUT",
+    path: "/books/{id}",
+    handler: editBookByIdHandler,
+    options: {
+      validate: {
+        payload: editSchema,
+        failAction,
+      },
+    },
+  },
+  {
+    method: "DELETE",
+    path: "/books/{id}",
+    handler: deleteBookByIdHandler,
   },
 ];
 
